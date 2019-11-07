744eb95c0a3de7cccfa7b946c39cc93112014080
Jesús Pérez <jjchiva94@hotmail.com>
2019-11-07 15:51:04 +0100

    Creacion BBDD

---
+++ src/BBDD/Comida.java
@@ -0,0 +1,41 @@
+package BBDD;
+
+public class Comida {
+    private String producto;
+    private int precio;
+    private int unidades;
+
+    public Comida(String producto, int precio, int unidades) {
+        this.producto = producto;
+        this.precio = precio;
+        this.unidades = unidades;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public String toString() {
+        return producto;
+    }
+}
